f459ae810b3c544ff4fd0561de78cd25f7bed085
Thiago Thaison <thiagothaison@gmail.com>
2022-09-19 14:16:11 -0300

    chore: add jest configs

---
+++ jest-integration.config.js
@@ -0,0 +1,4 @@
+const config = require("./jest.config");
+config.testMatch = ["<rootDir>/src/**/*.test.ts"];
+
+module.exports = config;
---
+++ jest-unit.config.js
@@ -0,0 +1,4 @@
+const config = require("./jest.config");
+config.testMatch = ["<rootDir>/src/**/*.spec.ts"];
+
+module.exports = config;
