dfadf9e19b3a5044afe519ddf59958106bcddfe7
Thiago Thaison <thiagothaison@gmail.com>
2022-09-19 16:00:31 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -92,4 +92,22 @@ describe("DbAddAccount UseCase", () => {
       password: "hashed-password",
     });
   });
+
+  test("Should throw if encrypter throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest.spyOn(addAccountRepositoryStub, "add").mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error());
+      })
+    );
+    const accountData = {
+      name: "valid name",
+      email: "valid@email.com",
+      password: "valid_password",
+    };
+
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
