c6bea9d2c4ca2565afcc0dbc91e62a23aba08f2f
Thiago Thaison <thiagothaison@gmail.com>
2022-09-19 11:41:49 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -25,11 +25,13 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (request.body.password !== request.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = request.body;
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
       }
 
-      const isValid = this.emailValidator.isValid(request.body.email);
+      const isValid = this.emailValidator.isValid(email);
 
       if (!isValid) {
         return badRequest(new InvalidParamError("email"));
