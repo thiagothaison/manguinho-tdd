d06ad570dd681f5262ae1db1a445bb48c022a403
Thiago Thaison <thiagothaison@gmail.com>
2022-09-19 10:17:43 -0300

    test: add customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from "../errors/missing-param-error";
 import { SignUpController } from "./signup";
 
 describe("SignUp Controller", () => {
@@ -13,7 +14,7 @@ describe("SignUp Controller", () => {
     const response = sut.handle(request);
 
     expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual(new Error("Missing param: name"));
+    expect(response.body).toEqual(new MissingParamError("name"));
   });
 
   test("Should return 400 if no email is provided", () => {
@@ -28,6 +29,6 @@ describe("SignUp Controller", () => {
     const response = sut.handle(request);
 
     expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual(new Error("Missing param: email"));
+    expect(response.body).toEqual(new MissingParamError("email"));
   });
 });
