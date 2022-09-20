c12d4a402e7cb5637fc0e8653dd3e07b352bab8e
Thiago Thaison <thiagothaison@gmail.com>
2022-09-20 02:38:22 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { mongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  test("Should return an account on success", async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+      name: "any name",
+      email: "any@email.com",
+      password: "any_password",
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any name");
+    expect(account.email).toBe("any@email.com");
+    expect(account.password).toBe("any_password");
+  });
+});
