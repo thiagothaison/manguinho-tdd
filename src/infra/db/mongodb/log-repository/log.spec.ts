fe1845f338719642cc070b5d223f18305c2f5d79
Thiago Thaison <thiagothaison@gmail.com>
2022-09-20 15:02:30 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,34 @@
+import { Collection } from "mongodb";
+
+import { mongoHelper } from "../helpers/mongo-helper";
+import { LogMongoRepository } from "./log";
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository();
+};
+
+describe("Log Mongo Repository", () => {
+  let errorCollection: Collection;
+
+  beforeAll(async () => {
+    await mongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    errorCollection = await mongoHelper.getCollection("errors");
+    await errorCollection.deleteMany({});
+  });
+
+  test("Should create an error log on success", async () => {
+    const sut = makeSut();
+    await sut.logError("any_error");
+
+    const count = await errorCollection.countDocuments();
+
+    expect(count).toBe(1);
+  });
+});
--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -36,7 +36,7 @@ const makeFakeAccount = (): AccountModel => ({
 
 const makeLogErrorRepository = (): LogErrorRepository => {
   class LogErrorRepositoryStub implements LogErrorRepository {
-    async log(stack: string): Promise<void> {
+    async logError(stack: string): Promise<void> {
       return new Promise((resolve) => resolve());
     }
   }
@@ -86,7 +86,7 @@ describe("LogController Decorator", () => {
   test("Should call LogErrorRepository with correct error if controller returns a server error", async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
 
-    const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+    const logSpy = jest.spyOn(logErrorRepositoryStub, "logError");
     jest
       .spyOn(controllerStub, "handle")
       .mockReturnValueOnce(
