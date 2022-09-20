5ab0ed6204003eb6eefeb1962d0978f049cc18c2
Thiago Thaison <thiagothaison@gmail.com>
2022-09-20 10:38:33 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+
+import app from "../config/app";
+
+describe("SignUp Routes", () => {
+  test("Should return an account on success", async () => {
+    await request(app)
+      .post("/api/signup")
+      .send({
+        name: "Fake Name",
+        email: "fake@email.com",
+        password: "fakePassword!@",
+        passwordConfirmation: "fakePassword!@",
+      })
+      .expect(200);
+  });
+});
