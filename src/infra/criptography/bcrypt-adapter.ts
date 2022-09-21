7ee21315a2c6d94b0722aabd0923991c4f9979ef
Thiago Thaison <thiagothaison@gmail.com>
2022-09-21 17:13:17 -0300

    refactor: move data layer interfaces to folders

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 
-import { Encrypter } from "../../data/protocols/encrypter";
+import { Encrypter } from "../../data/protocols/criptography/encrypter";
 
 export class BcrypterAdapter implements Encrypter {
   constructor(private readonly salt: number) {}
