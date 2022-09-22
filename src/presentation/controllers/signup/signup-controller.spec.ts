aba87196f045c7e7d7196880ecb9ea0fff05569c
Thiago Thaison <thiagothaison@gmail.com>
2022-09-22 15:27:16 -0300

    test: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,5 +1,10 @@
-import { MissingParamError, ServerError } from "../../errors";
-import { ok, serverError, badRequest } from "../../helpers/http/http-helper";
+import { ConflictError, MissingParamError, ServerError } from "../../errors";
+import {
+  ok,
+  serverError,
+  badRequest,
+  forbidden,
+} from "../../helpers/http/http-helper";
 import { SignUpController } from "./signup-controller";
 import {
   AccountModel,
@@ -159,5 +164,17 @@ describe("SignUp Controller", () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("Should return 403 if AddAccount returns null", async () => {
+    const { sut, addAccountStub } = makeSut();
+    jest
+      .spyOn(addAccountStub, "add")
+      .mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+
+    const request = makeFakeRequest();
+    const response = await sut.handle(request);
+
+    expect(response).toEqual(forbidden(new ConflictError("email")));
   });
 });
