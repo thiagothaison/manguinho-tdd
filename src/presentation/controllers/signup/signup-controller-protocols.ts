d3451f1e79b2c29afd20530314092af6e366bd98
Thiago Thaison <thiagothaison@gmail.com>
2022-09-22 15:11:53 -0300

    feat: ensure SignupController calls Authentication with correct values

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,6 +1,7 @@
 /* istanbul ignore file */
 export * from "../../protocols";
 export * from "../../protocols/email-validator";
+export * from "../../protocols/validation";
 export * from "../../../domain/use-cases/add-account";
 export * from "../../../domain/models/account";
-export * from "../../protocols/validation";
+export * from "../../../domain/use-cases/authentication";
