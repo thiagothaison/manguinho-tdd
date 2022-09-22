c07fc752e2482f856541f8e05885d82e44136e27
Thiago Thaison <thiagothaison@gmail.com>
2022-09-22 10:33:12 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/adapter/express/express-route-adapter.ts
+++ src/main/adapter/express/express-route-adapter.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
-import { Controller } from "../../presentation/protocols/controller";
-import { HttpRequest } from "../../presentation/protocols/http";
+import { Controller } from "../../../presentation/protocols/controller";
+import { HttpRequest } from "../../../presentation/protocols/http";
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
