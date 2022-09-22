db12f8263282618b46c0fc212423a54bd164a74f
Thiago Thaison <thiagothaison@gmail.com>
2022-09-22 11:40:19 -0300

    test: ensure login route return 200 if login was successfully

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,17 @@
+import { Collection } from "mongodb";
 import request from "supertest";
 
 import { mongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+
+let accountCollection: Collection;
+
+const makeFakeAccount = () => ({
+  name: "Fake Name",
+  email: "fake@email.com",
+  password: "fakePassword!@",
+  passwordConfirmation: "fakePassword!@",
+});
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -13,7 +23,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await mongoHelper.getCollection("accounts");
+    accountCollection = await mongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -21,11 +31,22 @@ describe("Login Routes", () => {
     test("Should return 200 on signup", async () => {
       await request(app)
         .post("/api/signup")
+        .send(makeFakeAccount())
+        .expect(200);
+    });
+  });
+
+  describe("POST /login", () => {
+    test("Should return 200 on login", async () => {
+      const fakeAccount = makeFakeAccount();
+
+      await request(app).post("/api/signup").send(fakeAccount);
+
+      await request(app)
+        .post("/api/login")
         .send({
-          name: "Fake Name",
-          email: "fake@email.com",
-          password: "fakePassword!@",
-          passwordConfirmation: "fakePassword!@",
+          email: fakeAccount.email,
+          password: fakeAccount.password,
         })
         .expect(200);
     });
