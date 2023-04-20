071467bc680af523229fab66ade402421f351205
Thiago Thaison <thiagothaison@gmail.com>
2023-04-20 13:59:45 -0300

    feat: other file

---
+++ src/data/use-cases/add-survey/other-file.ts
@@ -0,0 +1,12 @@
+import { AddSurveyModel } from "domain/use-cases/add-survey";
+
+import { AddSurvey, AddSurveyRepository } from "./db-add-survey-protocols";
+
+export class OtherFile implements AddSurvey {
+  constructor(private readonly addSurveyRepository: AddSurveyRepository) {}
+
+  async add(survey: AddSurveyModel): Promise<void> {
+    console.log("survey", survey);
+    await this.addSurveyRepository.add(survey);
+  }
+}
