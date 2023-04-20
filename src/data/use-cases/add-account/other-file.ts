eec98c0a5543980116db5f6ee987eb4b01704b35
Thiago Thaison <thiagothaison@gmail.com>
2023-04-20 19:17:10 -0300

    feat: another file

---
+++ src/data/use-cases/add-account/other-file.ts
@@ -0,0 +1,34 @@
+import {
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+  AddAccountRepository,
+  Hasher,
+  LoadAccountByEmailRepository,
+} from "./db-add-account-protocols";
+
+export class OtherFile implements AddAccount {
+  constructor(
+    private readonly hasher: Hasher,
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
+
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.hasher.hash(accountData.password);
+
+    const accountAlreadyExists =
+      await this.loadAccountByEmailRepository.loadByEmail(accountData.email);
+
+    if (accountAlreadyExists) return;
+
+    const account = await this.addAccountRepository.add({
+      ...accountData,
+      password: hashedPassword,
+    });
+
+    console.log('account', account)
+
+    return account;
+  }
+}
